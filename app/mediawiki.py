ba21196d6727989b09356b4ff5f12ec8366e73fd
ivanmijacika <imijacika20@stuy.edu>
2021-12-13 08:26:12 -0500

    started api call handler for mediawiki

---
+++ app/mediawiki.py
@@ -0,0 +1,20 @@
+# no-stall-GICS -- Christopher Liu, Ivan Mijacika, Shyne Choi, Gavin McGinley
+# SoftDev
+# P01 -- no-stock-GICS
+
+"""
+MediaWiki
+"""
+
+import requests
+
+def get_co_wiki(ticker: str) -> dict:
+    """Uses the MediaWiki API to retrieve a wiki summary based on the given
+    company name"""
+
+    api_request = requests.get(f"https://en.wikipedia.org/w/rest.php/v1/search/page?q={CONAME}&limit={NUMOFRESULTS}")
+    #should numofresults always be 1?
+    return api_request.json()
+
+if __name__ == "__main__":
+    print(get_co_wiki("Apple+Inc."))
