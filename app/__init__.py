73ad2f0a71cc2fef45cb690a03e84d569ba3649c
qiqimonkey4300 <yueqi04@gmail.com>
2021-12-13 10:45:34 -0500

    starter template

--- app/__init__.py
+++ app/__init__.py
@@ -4,3 +4,26 @@
 # [2022-01-03]
 
 from flask import Flask, render_template
+app = Flask(__name__)
+
+import requests
+
+# url = "https://yfapi.net/v6/finance/recommendationsbysymbol"
+#
+# querystring = {"symbols":"AAPL,BTC-USD,EURUSD=X"}
+#
+# headers = {
+#     'x-api-key': ""
+#     }
+#
+# response = requests.request("GET", url, headers=headers, params=querystring)
+#
+# print(response.text)
+
+@app.route("/")
+def homepage():
+     return render_template('index.html' )
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
