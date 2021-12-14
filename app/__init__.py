27a35fcef7f809b0a5220f54ded3ea49516f7a69
Christopher Liu <christopherliu@sorcero.com>
2021-12-14 08:22:17 -0500

    Add basic stock route placeholder

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 # no-stall-GICS — Christopher Liu, Ivan Mijacika, Shyne Choi, Gavin McGinley
 # SoftDev
 # P01 — no-stock-GICS
-# [2022-01-03]
-
-from flask import Flask, render_template
-app = Flask(__name__)
+# 2022-01-04
 
 import requests
+
+from flask import Flask, render_template
+
+from styvio import Stock
+
+app = Flask(__name__)
 
 # url = "https://yfapi.net/v6/finance/recommendationsbysymbol"
 #
@@ -20,9 +23,17 @@ import requests
 #
 # print(response.text)
 
+
 @app.route("/")
 def homepage():
-     return render_template('index.html' )
+    return render_template("index.html")
+
+
+@app.route("/stock/<ticker>")
+def stock(ticker):
+     stock_obj = Stock(ticker)
+     return stock_obj.get_short_name()
+
 
 if __name__ == "__main__":
     app.debug = True
--- app/mediawiki.py
+++ app/mediawiki.py
@@ -8,13 +8,17 @@ MediaWiki
 
 import requests
 
+
 def get_co_wiki(ticker: str) -> dict:
     """Uses the MediaWiki API to retrieve a wiki summary based on the given
     company name"""
 
-    api_request = requests.get(f"https://en.wikipedia.org/w/rest.php/v1/search/page?q={CONAME}&limit={NUMOFRESULTS}")
-    #should numofresults always be 1?
+    api_request = requests.get(
+        f"https://en.wikipedia.org/w/rest.php/v1/search/page?q={CONAME}&limit={NUMOFRESULTS}"
+    )
+    # should numofresults always be 1?
     return api_request.json()
+
 
 if __name__ == "__main__":
     print(get_co_wiki("Apple+Inc."))
